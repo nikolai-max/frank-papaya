7259ee39f2bc40f28407715ab9cc122f7851f8a8
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-05-06 16:14:51 +0200

    Added an counter up on the number

--- src/components/score/score.component.jsx
+++ src/components/score/score.component.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import { connect } from 'react-redux'
+import CountUp from 'react-countup';
 
 import './score.style.scss'
 
 const Score = ({currentScore}) => (
+
 <div className='score'>
-    <h1>{currentScore}</h1>
+    <h1><CountUp end={currentScore} /></h1>
+    
     <h2>Your score</h2>
 </div>
 )
