369acf7afba5913ce0dd7d447aa4f93c55bee771
Nikolai Toverud <nikolai.toverud@gmail.com>
2020-05-14 19:25:30 +0200

    Changed the state of keyPressed to en empty array and start an placeholder song  when 'init()'

--- src/functions/init.function.js
+++ src/functions/init.function.js
@@ -1,14 +1,20 @@
 import { connect } from 'react-redux'
 
 import { setCurrentScore } from '../redux/score/score.actions'
+import { pressedKeys } from '../redux/combination/combinations.actions'
+import Pappaya from '../media/sounds/pappaya.mp3'
 
 export let init = (dispatch) => {
+    const audio = new Audio(Pappaya)
     // Reset the state of score and keyPressed
     dispatch(setCurrentScore(0))
     console.log('init')
     // Initiate the timer state
-    // Start recording
+    dispatch(pressedKeys([]))
     // Start playing the frank papaya tune
+    audio.play()
+    console.log(audio)
+    // Start recording
 }
 
 const mapDispatchToProps = dispatch => ({
